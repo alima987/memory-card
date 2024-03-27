6fa7dbd281e16064ed7b53caa9e45e084d723269
alima987 <alima1996b@gmail.com>
2024-03-27 16:33:22 +0500

    feat: add .

--- memory card/src/App.tsx
+++ memory card/src/App.tsx
@@ -3,36 +3,52 @@ import './App.css'
 import Cards from './components/Cards/Cards'
 import Header from './components/Hearder/Header'
 import { Characters } from './components/Cards/Cards'
-import RandomCards  from './components/RandomCards/RandomCards'
 
 function App() {
 
   const [cards, setCards] = useState<Characters[]>([])
-  const [randomCards, setRandomCards] = useState<Characters[]>([])
 
-  const getApiData = useCallback( 
+  const getApiData = useCallback(
     async () => {
+      try {
         const baseURL = 'https://rickandmortyapi.com/api/character';
-        const response = await fetch(baseURL).then((response) => response.json())
-
-        setCards(response.results)
-
-  }, [])
+        const response = await fetch(baseURL);
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        const cardResult = data.results
+        setCards(cardResult);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }, [])
 
   useEffect(() => {
     getApiData();
   }, [getApiData])
 
-  const randomNum = () => {
-    const randomedCards = [...cards].sort(() => Math.random - 0.5)
-    setRandomCards(randomedCards)
+  const shuffle = () => {
+    let currentI = cards.length - 1
+    let mixedCards = [...cards]
+    let j
+    while (currentI > 0) {
+      j = Math.floor(Math.random() * (currentI + 1))
+      currentI --
+      [mixedCards[currentI], mixedCards[j]] = [mixedCards[j], mixedCards[currentI]]
+    }
+    return setCards(mixedCards)
   }
+
+  const handleClick = () => {
+    shuffle();
+  }
+
 
   return (
     <>
     <Header/>
-    <Cards cards={cards} onCardClick={randomNum}/>
-    <RandomCards randomCards={randomCards}/>
+    <Cards cards={cards} onCardClick={() => handleClick()}/>
     </>
   )
 }
